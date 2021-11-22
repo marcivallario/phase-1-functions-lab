fadae0ef05467005b87df0a08afed5b7217f1159
Marci Vallario <marcivallario@gmail.com>
2021-11-22 17:28:30 -0500

    commit tree 8bf6e29ed30975d2b17acd2c95e628137c665190

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location) {
+    let blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let travel = Math.abs(destination-start);
+    return travel * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet > 0 && feet <= 400) {
+        return 0;
+    } else if (feet > 400 && feet < 2000) {
+        feet = feet - 400;
+        return feet * 0.02;
+    } else if (feet > 2000  && feet < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
